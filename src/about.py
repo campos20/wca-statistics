c09810b9177233589a4a71413ac072af5479aae8
campos20 <camposalexandreh@gmail.com>
2019-03-16 11:57:47 -0300

    Split html again

--- src/about.py
+++ src/about.py
@@ -3,12 +3,13 @@ from utils import html_link_format
 def main():
 
 	template = open("template/about.html", "r", encoding="utf8").read()
-	text = open("template/about_text.txt", "r", encoding="utf8").read()
 	header = open("template/header.html", "r", encoding="utf8").read()
+	top = open("template/top.html", "r", encoding="utf8").read()	
 	left_bar = open("template/left_bar.html", "r", encoding="utf8").read()
+	text = open("template/about_text.txt", "r", encoding="utf8").read()
 	closing = open("template/closing.html", "r", encoding="utf8").read()
 		
-	page = template%(header, left_bar, text, closing)
+	page = template%(header, top, left_bar, text, closing)
 
 	with open("pages/about.html", "w", encoding="utf8") as fout:
 		fout.write(page)
--- src/index.py
+++ src/index.py
@@ -14,6 +14,7 @@ def main():
 	
 	template = open("template/index.html", "r", encoding="utf8").read()
 	header = open("template/header.html", "r", encoding="utf8").read()
+	top = open("template/top.html", "r", encoding="utf8").read()	
 	left_bar = open("template/left_bar.html", "r", encoding="utf8").read()
 	closing = open("template/closing.html", "r", encoding="utf8").read()
 	
@@ -40,7 +41,7 @@ def main():
 		content += y
 	content += '</ul>'
 		
-	page = template%(header, left_bar, date_stamp, content, closing)
+	page = template%(header, top, left_bar, date_stamp, content, closing)
 
 	with open("pages/index.html", "w", encoding="utf8") as fout:
 		fout.write(page)
