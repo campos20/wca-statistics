7a36e4d41cbcd377010e8ca0ffbcc82038a94cda
campos20 <camposalexandreh@gmail.com>
2019-04-04 18:54:27 -0300

    Add stat ratio active competitors/delegates, add mysql support
    Add mysql support and 2 stats about delegates (#5)
    
    Add mysql and 2 stats about delegates
    
    Fix broken database generation
    
    Fix broken links

---
+++ generate-output-from-database.sh
@@ -0,0 +1,18 @@
+if ! sudo bash -c '[[ -d "/var/lib/mysql/WCA" ]]'
+then
+	echo "Building the database. This will take a while."
+	sudo mysql -e "CREATE DATABASE WCA; USE WCA; SOURCE wca-developer-database-dump.sql;"
+	echo "Finally done."
+fi
+
+# Create the temp
+if [ ! -d database_out ]; then
+    mkdir database_out
+fi
+
+
+# Generate output for the Statistics
+
+
+# CountryName NumberOfDelegates
+sudo mysql --disable-column-names -e "USE WCA; SELECT Countries.id, COUNT(Countries.name) FROM users JOIN Countries WHERE users.country_iso2 = Countries.iso2 AND users.delegate_status IS NOT NULL GROUP BY Countries.id;" > database_out/stat-database-ratio_delegates_competitors_last_year.txt
---
+++ src/stat-database-active_countries_with_no_delegates.py
@@ -0,0 +1,88 @@
+import bisect, csv, datetime, sys
+from build_page import build_results
+
+# This is basically the NA countries from stat-database-ratio_delegates_competitors_last_year.py
+
+def no_delegates():
+
+	days = 365
+	
+	countries_delegates = []
+
+	# the file is there already, so we can use it
+	file = open("database_out/stat-database-ratio_delegates_competitors_last_year.txt", "r").readlines()
+
+	for line in file:
+		temp = line.split()
+		delegates = int(temp[-1])
+		if delegates>0: # Avoid countries with 0 delegates. This prevents 0 division
+			country = " ".join(temp[:-1])
+
+			i = bisect.bisect_left(countries_delegates, country) # USA messes with ordering (capitalization), so here we sort this again (mysql isn't case sensitive for this matter)
+			countries_delegates.insert(i, country)
+
+	competitors_last_year = []
+
+	today = datetime.date.today()
+
+	countries_persons = []
+	competitors_id = []
+	competitors_names = []
+
+	tsvfile = open("WCA_export/WCA_export_Results_Ordered.tsv", "r")
+	tsvreader = csv.reader(tsvfile, delimiter="\t")
+	next(tsvreader, None) # skip header
+	for line in tsvreader: # bisect here?
+		year, month, day = map(int, [line[17], line[18], line[19]])
+		date = datetime.date(year, month, day)
+		if date + datetime.timedelta(days=days) >= today:
+			person_name = line[6]
+			person_id = line[7]
+			country = line[8]
+
+			i = bisect.bisect_left(countries_persons, country)
+			if i == len(countries_persons) or countries_persons[i] != country:
+				countries_persons.insert(i, country)
+				competitors_id.insert(i, [])
+				competitors_names.insert(i, [])
+
+			j = bisect.bisect_left(competitors_id[i], person_id)
+			if j == len(competitors_id[i]) or competitors_id[i][j] != person_id:
+				competitors_id[i].insert(j, person_id)
+				competitors_names[i].insert(j, person_name)
+
+	table = []
+	no_delegates_country = []
+	competitors_no_delegates = []
+	for i in range(len(countries_persons)):
+		country = countries_persons[i]
+		j = bisect.bisect_left(countries_delegates, country)
+
+		if j == len(countries_delegates) or countries_delegates[j] != country:
+			no_delegates_country.append(country)
+			competitors_no_delegates.append(len(competitors_id[i]))
+
+	c = 1
+	prev = None
+	for competitors, country in sorted(zip(competitors_no_delegates, no_delegates_country))[::-1]:
+		pos = c
+		if competitors == prev:
+			pos = "-"
+		table.append([pos, competitors, country])
+		c += 1
+		prev = competitors
+
+	out = {}
+	out["title"] = "Active countries with no delegates in the last %s days"%days
+	out["labels"] = ["Pos", "Active competitors", "Country"]
+	out["explanation"] = 'A country is considered active here if a competitor from that country has competed in the past %s days. <a href="stat-database-ratio_delegates_competitors_last_year.html">Related</a>.'%days
+	out["table"] = table
+	return out
+
+def main():
+	args = sys.argv
+	out = no_delegates()
+	build_results(out, args)
+
+if __name__ == "__main__":
+	main()
