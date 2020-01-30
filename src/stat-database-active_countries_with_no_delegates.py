9559ef97339b455785b1d7b6557e15d4dd3f69f7
Alexandre Campos <camposalexandreh@gmail.com>
2020-01-29 21:40:55 -0300

    Fix active countries with no delegates

--- src/stat-database-active_countries_with_no_delegates.py
+++ src/stat-database-active_countries_with_no_delegates.py
@@ -1,88 +1,109 @@
-import bisect, csv, datetime, sys
+import bisect
+import csv
+import datetime
+import sys
 from build_page import build_results
+from utils import get_delegates_list, iso2_country_name
 
 # This is basically the NA countries from stat-database-ratio_delegates_competitors_last_year.py
 
+
 def no_delegates():
+    days = 365
 
-	days = 365
-	
-	countries_delegates = []
+    print("Getting delegate list")
+    delegates_list = get_delegates_list()
 
-	# the file is there already, so we can use it
-	file = open("database_out/stat-database-ratio_delegates_competitors_last_year.txt", "r").readlines()
+    countries_delegates = list(map(iso2_country_name, list(
+        set(map(lambda x: x["country_iso2"], delegates_list)))))
+    print("Number of countries with at least 1 delegate: %s" %
+          len(countries_delegates))
 
-	for line in file:
-		temp = line.split()
-		delegates = int(temp[-1])
-		if delegates>0: # Avoid countries with 0 delegates. This prevents 0 division
-			country = " ".join(temp[:-1])
+    countries_delegates.sort()
 
-			i = bisect.bisect_left(countries_delegates, country) # USA messes with ordering (capitalization), so here we sort this again (mysql isn't case sensitive for this matter)
-			countries_delegates.insert(i, country)
+    # Competitors from United States are recorded as USA. This messes by saying that USA has no delegates.
+    # We replace 'United States' with USA.
+    usa_long_name = "United States"
+    j = bisect.bisect_left(countries_delegates, usa_long_name)
+    if j < len(countries_delegates) and countries_delegates[j] == usa_long_name:
+        countries_delegates[j] = "USA"
+        countries_delegates.sort()
 
-	competitors_last_year = []
+    today = datetime.date.today()
 
-	today = datetime.date.today()
+    active_countries_competitors_list = []
 
-	countries_persons = []
-	competitors_id = []
-	competitors_names = []
+    print("Searching the results for active countries")
+    active_countries = []
+    tsvfile = open("WCA_export/WCA_export_Results_Ordered.tsv", "r")
+    tsvreader = csv.reader(tsvfile, delimiter="\t")
+    next(tsvreader, None)  # skip header
+    for line in tsvreader:  # bisect here?
+        year, month, day = map(int, [line[17], line[18], line[19]])
+        date = datetime.date(year, month, day)
+        if date + datetime.timedelta(days=days) < today:
+            continue
 
-	tsvfile = open("WCA_export/WCA_export_Results_Ordered.tsv", "r")
-	tsvreader = csv.reader(tsvfile, delimiter="\t")
-	next(tsvreader, None) # skip header
-	for line in tsvreader: # bisect here?
-		year, month, day = map(int, [line[17], line[18], line[19]])
-		date = datetime.date(year, month, day)
-		if date + datetime.timedelta(days=days) >= today:
-			person_name = line[6]
-			person_id = line[7]
-			country = line[8]
+        wca_id = line[7]
+        country = line[8]
 
-			i = bisect.bisect_left(countries_persons, country)
-			if i == len(countries_persons) or countries_persons[i] != country:
-				countries_persons.insert(i, country)
-				competitors_id.insert(i, [])
-				competitors_names.insert(i, [])
+        i = bisect.bisect_left(active_countries, country)
 
-			j = bisect.bisect_left(competitors_id[i], person_id)
-			if j == len(competitors_id[i]) or competitors_id[i][j] != person_id:
-				competitors_id[i].insert(j, person_id)
-				competitors_names[i].insert(j, person_name)
+        # New active country found
+        if i == len(active_countries) or active_countries[i] != country:
+            active_countries.insert(i, country)
+            active_countries_competitors_list.insert(i, [wca_id])
+        else:
+            # If the country already exists,
+            j = bisect.bisect_left(
+                active_countries_competitors_list[i], wca_id)
+            if j == len(active_countries_competitors_list[i]) or active_countries_competitors_list[i][j] != wca_id:
+                active_countries_competitors_list[i].insert(j, wca_id)
 
-	table = []
-	no_delegates_country = []
-	competitors_no_delegates = []
-	for i in range(len(countries_persons)):
-		country = countries_persons[i]
-		j = bisect.bisect_left(countries_delegates, country)
+    print("Number of active countries: %s" % len(active_countries))
 
-		if j == len(countries_delegates) or countries_delegates[j] != country:
-			no_delegates_country.append(country)
-			competitors_no_delegates.append(len(competitors_id[i]))
+    print("Searching for active countries with no delegates")
+    table = []
+    no_delegates_country = []
+    no_delegates_country_competitor_count = []
+    for i in range(len(active_countries)):
+        country = active_countries[i]
+        j = bisect.bisect_left(countries_delegates, country)
 
-	c = 1
-	prev = None
-	for competitors, country in sorted(zip(competitors_no_delegates, no_delegates_country))[::-1]:
-		pos = c
-		if competitors == prev:
-			pos = "-"
-		table.append([pos, competitors, country])
-		c += 1
-		prev = competitors
+        if j == len(countries_delegates) or countries_delegates[j] != country:
+            no_delegates_country.append(country)
+            no_delegates_country_competitor_count.append(
+                len(active_countries_competitors_list[i]))
 
-	out = {}
-	out["title"] = "Active countries with no delegates in the last %s days"%days
-	out["labels"] = ["Pos", "Active competitors", "Country"]
-	out["explanation"] = 'A country is considered active here if a competitor from that country has competed in the past %s days. <a href="stat-database-ratio_delegates_competitors_last_year.html">Related</a>.'%days
-	out["table"] = table
-	return out
+    print("Building table")
+    c = 1
+    prev = None
+    for competitors, country in sorted(zip(no_delegates_country_competitor_count, no_delegates_country))[::-1]:
+        pos = c
+        if competitors == prev:
+            pos = "-"
+        table.append([pos, competitors, country])
+        c += 1
+        prev = competitors
+
+    out = {}
+    out["title"] = "Active countries with no delegates in the last %s days" % days
+    out["labels"] = ["Pos", "Active competitors", "Country"]
+    out["explanation"] = 'A country is considered active here if a competitor from that country has competed in the past %s days. <a href="stat-database-ratio_delegates_competitors_last_year.html">Related</a>.' % days
+    out["table"] = table
+
+    return out
+
 
 def main():
-	args = sys.argv
-	out = no_delegates()
-	build_results(out, args)
+    args = sys.argv
+
+    print("Active countries with no delegates")
+    out = no_delegates()
+
+    print("Building page")
+    build_results(out, args)
+
 
 if __name__ == "__main__":
-	main()
+    main()
